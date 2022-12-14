cb5bbec0290e3c0b88056c3b535d7c0e024b6eb9
aldysaptra05 <aldysaptra05@gmail.com>
2022-12-14 16:04:07 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae consequuntur asperiores a accusantium. Quo quod ipsa, earum voluptatum facere eveniet suscipit labore aliquid asperiores, quaerat minima exercitationem amet aliquam dicta.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
